d2b109a1f030e65ad399b4d623a14224e3dbc402
Erik on Apollo <ewfalor@gmail.com>
2017-02-22 12:15:39 -0700

    creates a tree-like structure, it just doesn't look like it

--- scratchpad.js
+++ scratchpad.js
@@ -6,56 +6,37 @@
  * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
-
-
-var addText = function (sel, text) {
-	var el = document.querySelector(sel);
-	if (el.childNodes.length === 0) {
-		var p = document.createElement('p');
-		p.textContent = text;
-		el.appendChild(p)
-	}
+function fibHelper(n) {
+  var value;
+  var div = document.createElement('div');
+  // leaf nodes aka. base case
+  if (n < 2) {
+    if (n === 0) {
+      value = 0;
+    } 
+    else if (n === 1) {
+      value = 1;
+    }
+    var p = document.createElement('p');
+    p.textContent = 'Fib(' + n + ') = ' + value;
+    div.appendChild(p)
+  } 
+  else {
+    var left = fibHelper(n - 1);
+    var right = fibHelper(n - 2);
+   
+    value = left.value + right.value;
+    var p = document.createElement('p');
+    p.textContent = 'Fib(' + n + ') = ' + value;
+    div.appendChild(p);
+    
+    div.appendChild(left.html);
+    div.appendChild(right.html);
+  }
+  return { 'value': value, 'html': div };
 }
-;['yellow', 'blue', 'green', 'red'].map(
-	function (color) {
-	console.log(color);
-	addText("." + color, color + " box")
-});
-
-var fib = function fibHelper(n) {
-	if (n === 0) {
-		return 0
-	}
-	else if (n === 1) {
-		return 1
-	}
-	else {
-		var first = fibHelper(n - 2)
-		var second = fibHelper(n - 1)
-		console.log("first=" + first + " second=" + second)
-		return 	first + second
-		}
+var fib = function (n, node) {
+  var tree = fibHelper(n)
+  node.appendChild(tree.html)
 }
-
-var red = document.querySelector('.red')
-
-var p = document.createElement('p')
-red.appendChild(p)
-
-var array = []
-for (var i = 0; i < 11; i++) {
-	array[i] = fib(i)
-}
-
-p.textContent = array.join(", ")
-
-
-/*
-Exception: TypeError: el is null
-addText@Scratchpad/1:13:1
-@Scratchpad/1:22:2
-@Scratchpad/1:19:2
-*/
-/*
-undefined
-*/
+fib(6, document.querySelector('.red'))
